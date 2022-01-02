873a6998d7f556c26a467ae18793e64f39e92a68
Saurabh Jain <jsourabh861@gmail.com>
2022-01-02 23:24:23 +0530

    Create Decode_the_string.py
    An encoded string (s) is given, the task is to decode it. The pattern in which the strings were encoded were as follows

---
+++ Decode_the_string.py
@@ -0,0 +1,38 @@
+
+class Solution:
+    def decodedString(self, s):
+        # code here
+        stack = []
+        curString = ""
+
+        curNum = 0
+
+        for c in s:
+            if c == '[':
+                stack.append(curString)
+                stack.append(curNum)
+                curString = ''
+                curNum = 0
+            elif c == ']':
+                num = stack.pop()
+                prevString = stack.pop()
+                curString = prevString + num*curString
+            elif c.isdigit():
+                curNum = curNum*10 + int(c)
+            else:
+                curString += c
+        return curString
+
+
+#{
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        s = input()
+
+        ob = Solution()
+        print(ob.decodedString(s))
+# } Driver Code Ends
