7cb5bde84aa710ed42bc2e99d345fc1d0c2e1dff
Saurabh Jain <jsourabh861@gmail.com>
2022-01-26 19:50:34 +0530

    Create construct_the_quad_tree.py

---
+++ Intuit/construct_the_quad_tree.py
@@ -0,0 +1,17 @@
+class Solution:
+    def construct(self, grid: 'List[List[int]]') -> 'Node':
+        root = Node(True, True, None, None, None, None)
+        if len(set([item for row in grid for item in row])) == 1:
+            root.val = bool(grid[0][0])
+        else:
+            root.isLeaf = False
+            size = len(grid)
+            root.topLeft = self.construct(
+                [row[:size//2] for row in grid[:size//2]])
+            root.topRight = self.construct(
+                [row[size//2:] for row in grid[:size//2]])
+            root.bottomLeft = self.construct(
+                [row[:size//2] for row in grid[size//2:]])
+            root.bottomRight = self.construct(
+                [row[size//2:] for row in grid[size//2:]])
+        return root
